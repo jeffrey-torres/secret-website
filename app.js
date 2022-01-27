6aea9bfad80e03f102ca5c0837095f5016aef9dc
Jeffrey Torres <oceanmks@gmail.com>
2022-01-26 21:31:16 -0500

    Use md5 module instead of mongoose-encryption

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const ejs = require('ejs');
 const mongoose = require('mongoose');
-const encrypt = require('mongoose-encryption');
+const md5 = require('md5');
 
 const app = express();
 
@@ -19,9 +19,6 @@ const userSchema = new mongoose.Schema({
   email : String,
   password: String
 });
-
-// Encrypt secret message and password attribute of the user schema
-userSchema.plugin(encrypt, { secret : process.env.SECRET, encryptedFields: ["password"]});
 
 const User = new mongoose.model("User", userSchema);
 
@@ -44,7 +41,7 @@ app.get("/logout", (req, res) => {
 // Check if user login info exists in the database
 app.post("/login", (req, res) => {
   const username = req.body.username;
-  const password = req.body.password;
+  const password = md5(req.body.password);
 
   User.findOne({email: username}, (err, foundUser) => {
     // if user doesn't exist in the DB, throw an error
@@ -67,7 +64,7 @@ app.post("/login", (req, res) => {
 app.post("/register", (req, res) => {
   const newUser = new User({
     email : req.body.username,
-    password : req.body.password
+    password : md5(req.body.password);
   });
 
   newUser.save((err) => {
