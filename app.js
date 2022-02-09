75b063349bbb18799f8d695705c57d6028d10215
Jeffrey Torres <oceanmks@gmail.com>
2022-02-08 20:26:14 -0500

    Add functionality to secrets page

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(session({
-  secret : "iloveyouyamyams",
+  secret : process.env.SECRET,
   resave : false,
   saveUninitialized : false
 }));
@@ -36,7 +36,8 @@ const userSchema = new mongoose.Schema({
   password: String,
   facebookId: String,
   googleId: String,
-  redditId: String
+  redditId: String,
+  secret: String
 });
 
 userSchema.plugin(passportLocalMongoose);
@@ -56,6 +57,7 @@ passport.deserializeUser((id, done) => {
   });
 });
 
+// Try to authenticate users using their Google account
 passport.use(new GoogleStrategy({
     clientID: process.env.CLIENT_ID,
     clientSecret: process.env.CLIENT_SECRET,
@@ -71,10 +73,12 @@ passport.use(new GoogleStrategy({
   }
 ));
 
+// Try to authenticate users using their Facebook account
 passport.use(new FacebookStrategy({
     clientID: process.env.APP_ID,
     clientSecret: process.env.APP_SECRET,
-    callbackURL: "http://localhost:3000/auth/facebook/secrets"
+    callbackURL: "http://localhost:3000/auth/facebook/secrets",
+    profileFields: ['id', 'displayName']
   },
   function(accessToken, refreshToken, profile, cb) {
     console.log(profile);
@@ -85,6 +89,7 @@ passport.use(new FacebookStrategy({
   }
 ));
 
+// Try to authenticate users using their Reddit account
 passport.use(new RedditStrategy({
     clientID: process.env.REDDIT_CONSUMER_KEY,
     clientSecret: process.env.REDDIT_CONSUMER_SECRET,
@@ -109,7 +114,7 @@ app.get("/auth/facebook",
 app.get("/auth/facebook/secrets",
   passport.authenticate("facebook", { failureRedirect: "/login" }),
   function(req, res) {
-    // Successful authentication, redirect to Secrets page.
+    // Successful authentication, redirect to Secrets page
     res.redirect("/secrets");
 });
 
@@ -120,7 +125,7 @@ app.get("/auth/google",
 app.get("/auth/google/secrets",
   passport.authenticate("google", { failureRedirect: "/login" }),
   function(req, res) {
-    // Successful authentication, redirect to Secrets page.
+    // Successful authentication, redirect to Secrets page
     res.redirect("/secrets");
 });
 
@@ -135,6 +140,7 @@ app.get("/auth/reddit/secrets", function(req, res, next){
   // Check for origin via state token
   if (req.query.state == req.session.state){
     passport.authenticate("reddit", {
+      // Successful authentication, redirect to Secrets page
       successRedirect: "/secrets",
       failureRedirect: "/login"
     })(req, res, next);
@@ -160,8 +166,20 @@ app.get("/logout", (req, res) => {
 // Display the secrets page if a cookie for the user's credentials
 // is present when they enter localhost:3000/secrets
 app.get("/secrets", (req, res) => {
+  User.find({"secret": {$ne : null}}, (err, foundUsers) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUsers) {
+        res.render("secrets", {usersWithSecrets: foundUsers});
+      }
+    }
+  });
+});
+
+app.get("/submit", (req, res) => {
   if (req.isAuthenticated()) {
-    res.render("secrets");
+    res.render("submit");
   } else {
     res.redirect("/login");
   }
@@ -198,6 +216,26 @@ app.post("/register", (req, res) => {
       passport.authenticate("local")(req, res, () => {
         res.redirect("secrets");
       });
+    }
+  });
+});
+
+// Store and save a secret that the user submitted
+app.post("/submit", (req, res) => {
+  const submittedSecret = req.body.secret;
+
+  console.log(req.user.id);
+
+  User.findById(req.user.id, (err, foundUser) => {
+    if (err) {
+      console.log(err);
+    } else {
+      if (foundUser) {
+        foundUser.secret = submittedSecret;
+        foundUser.save(() => {
+          res.redirect("/secrets");
+        });
+      }
     }
   });
 });
